29938bea9bda609227ea97934a3ff103ef2f9516
Tanuj <khiladitanuj@gmail.com>
2024-09-06 18:46:38 +0530

    import statement fixed

--- src/Pages/Netflix.jsx
+++ src/Pages/Netflix.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Banner from "../Components/Banner";
-import Navbar from "../Components/NavBar";
+import Navbar from "../Components/Navbar";
 import request from "../request";
 import Row from "../Components/Row";
 
